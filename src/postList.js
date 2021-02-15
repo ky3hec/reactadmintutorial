15ddd89a4bff248bbd338c7c975172007227699e
Max <maksim82@hotmail.com>
2021-02-15 14:45:09 +0200

    Tutorial. Added search and filters functionalty

--- src/postList.js
+++ src/postList.js
@@ -1,14 +1,29 @@
 import * as React from "react";
 import {
   List,
+  Filter,
+  TextInput,
+  ReferenceInput,
+  SelectInput,
   Datagrid,
   TextField,
   ReferenceField,
   EditButton,
 } from "react-admin";
 
+
+const PostFilter = (props) => (
+  <Filter {...props}>
+      <TextInput label="Search" source="q" alwaysOn />
+      <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+          <SelectInput optionText="name" />
+      </ReferenceInput>
+  </Filter>
+);
+
+
 export const PostList = (props) => (
-  <List {...props}>
+  <List filters={<PostFilter/>}{...props}>
     <Datagrid>
       <TextField source="id" />
       <ReferenceField source="userId" reference="users">
